ccdea5ce0c6176831763f3929d1c7ea63c0b1670
Julien <julien.quennehen@edu.devinci.fr>
2022-07-27 11:24:18 +0200

    feat: 🎸 add of prisma and SQLite

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,4 +1,12 @@
-import { objectType } from "nexus";
+import {
+  extendType,
+  nonNull,
+  objectType,
+  stringArg,
+  list,
+  intArg,
+} from "nexus";
+import { NexusGenObjects } from "../../nexus-typegen";
 
 export const Link = objectType({
   name: "Link",
@@ -6,5 +14,56 @@ export const Link = objectType({
     t.nonNull.int("id");
     t.nonNull.string("description");
     t.nonNull.string("url");
+  },
+});
+
+let links: NexusGenObjects["Link"][] = [
+  {
+    id: 1,
+    url: "www.howtographql.com",
+    description: "Fullstack tutoeeeerial for GraphQL",
+  },
+  {
+    id: 2,
+    url: "graphql.org",
+    description: "GraphQL official website",
+  },
+];
+
+export const LinkQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.nonNull.list.nonNull.field("feed", {
+      type: "Link",
+      resolve(parent, args, context, info) {
+        return links;
+      },
+    });
+  },
+});
+
+export const LinkMutation = extendType({
+  type: "Mutation",
+  definition(t) {
+    t.nonNull.field("post", {
+      type: "Link",
+      args: {
+        description: nonNull(stringArg()),
+        url: nonNull(stringArg()),
+      },
+
+      resolve(parent, args, context) {
+        const { description, url } = args;
+
+        let idCount = links.length + 1;
+        const link = {
+          id: idCount,
+          description: description,
+          url: url,
+        };
+        links.push(link);
+        return link;
+      },
+    });
   },
 });
